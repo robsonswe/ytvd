8b81d39da1325c3c354b8d29f6adca4516a815a7
Robson Santana <41270745+ROSS1996@users.noreply.github.com>
2025-03-25 09:53:05 -0300

    Compile update tool

--- run.bat
+++ run.bat
@@ -1,8 +1,20 @@
 @echo off
+cd /d "%~dp0"
+
+echo Executando script de atualizacao...
+update-tool.exe ^
+    --ytvd-folder YTVD2 ^
+    --version-file version.txt ^
+    --zip-url https://github.com/robsonswe/ytvd/releases/latest/download/YTVD2.zip ^
+    --github-api-url https://api.github.com/repos/robsonswe/ytvd/releases/latest
+if %ERRORLEVEL% NEQ 0 (
+    echo Erro: O script de atualizacao falhou. Verifique os logs acima.
+    pause
+    exit /b %ERRORLEVEL%
+)
+
+echo ==============================
+echo Iniciando a aplicacao...
+echo ==============================
 cd /d "%~dp0\YTVD2"
-
-echo ==============================
-echo Iniciando YouTubeDownloader...
-echo ==============================
-
 start "" "YouTubeDownloader.exe"
