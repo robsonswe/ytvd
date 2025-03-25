c586a4279def68716ee760178ce818dda45c17dd
Robson Santana <41270745+ROSS1996@users.noreply.github.com>
2025-03-25 10:04:01 -0300

    Adjust update script

--- run.bat
+++ run.bat
@@ -1,6 +1,22 @@
 @echo off
 cd /d "%~dp0"
 
+:check_git_updates
+echo Verificando atualizacoes do repositorio...
+echo ==============================
+git pull > git_update_check.txt 2>&1
+findstr /C:"Already up to date." git_update_check.txt >nul
+if %ERRORLEVEL% NEQ 0 (
+    echo Atualizacoes encontradas no repositorio Git. Reiniciando o script...
+    del git_update_check.txt
+    timeout /t 2 /nobreak >nul
+    start "" "%~f0"
+    exit
+)
+del git_update_check.txt
+echo Repositorio ja esta atualizado.
+echo.
+
 echo Executando script de atualizacao...
 update-tool.exe ^
     --ytvd-folder YTVD2 ^
@@ -13,7 +29,7 @@ if %ERRORLEVEL% NEQ 0 (
     exit /b %ERRORLEVEL%
 )
 
-echo ==============================
+echo.
 echo Iniciando a aplicacao...
 echo ==============================
 cd /d "%~dp0\YTVD2"
