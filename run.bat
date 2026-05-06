b5b034b5c3a104791241e9db8c213327a103d528
Robson Santana <41270745+ROSS1996@users.noreply.github.com>
2026-05-05 21:06:45 -0300

    updated the update tool

--- run.bat
+++ run.bat
@@ -32,7 +32,7 @@ update-tool.exe ^
     --ytvd-folder YTVD2 ^
     --version-file version.txt ^
     --zip-url https://github.com/robsonswe/ytvd/releases/latest/download/YTVD2.zip ^
-    --github-api-url https://api.github.com/repos/robsonswe/ytvd/releases/latest
+    --api-url https://api.github.com/repos/robsonswe/ytvd/releases/latest
 if %ERRORLEVEL% NEQ 0 (
     echo Erro: O script de atualizacao falhou. Verifique os logs acima.
     pause
